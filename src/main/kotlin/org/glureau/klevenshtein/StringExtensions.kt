b9fac0c19d8496bd2f9b222f8dfd24ceb3b29dbd
Gregory Lureau <gregorylureau@gmail.com>
2016-03-11 19:47:20 +0100

    Initial commit.

---
+++ src/main/kotlin/org/glureau/klevenshtein/StringExtensions.kt
@@ -0,0 +1,32 @@
+package org.glureau.klevenshtein
+
+/**
+ * Created by Greg on 11/03/2016.
+ */
+fun CharSequence.levenshtein(rhs: CharSequence): Int {
+    val lhsLength = this.length
+    val rhsLength = rhs.length
+
+    var cost = Array(lhsLength) { it }
+    var newCost = Array(lhsLength) { 0 }
+
+    for (i in 1..rhsLength - 1) {
+        newCost[0] = i
+
+        for (j in 1..lhsLength - 1) {
+            val match = if (this[j - 1] == rhs[i - 1]) 0 else 1
+
+            val costReplace = cost[j - 1] + match
+            val costInsert = cost[j] + 1
+            val costDelete = newCost[j - 1] + 1
+
+            newCost[j] = Math.min(Math.min(costInsert, costDelete), costReplace)
+        }
+
+        val swap = cost
+        cost = newCost
+        newCost = swap
+    }
+
+    return cost[lhsLength - 1]
+}
